c0ac82951ca19622cc000c7cf06a13ff16e30c2c
mike84265 <b03901078@ntu.edu.tw>
2018-04-20 03:40:55 +0800

    [HW1] ver 2.8 put back the model_init.txt

--- HW1/c_cpp/train.sh
+++ HW1/c_cpp/train.sh
@@ -5,8 +5,8 @@ if [ -z $ITER ]; then
     exit 1;
 fi
 
-./train $ITER model_init.txt ../seq_model_01.txt model_01.txt 
-./train $ITER model_init.txt ../seq_model_02.txt model_02.txt 
-./train $ITER model_init.txt ../seq_model_03.txt model_03.txt 
-./train $ITER model_init.txt ../seq_model_04.txt model_04.txt 
-./train $ITER model_init.txt ../seq_model_05.txt model_05.txt 
+./train $ITER ../model_init.txt ../seq_model_01.txt model_01.txt 
+./train $ITER ../model_init.txt ../seq_model_02.txt model_02.txt 
+./train $ITER ../model_init.txt ../seq_model_03.txt model_03.txt 
+./train $ITER ../model_init.txt ../seq_model_04.txt model_04.txt 
+./train $ITER ../model_init.txt ../seq_model_05.txt model_05.txt 
